87c8c411b86f66138957bf4be5bb37713a11d5e5
gennadynemchin@gmail.com <gennadynemchin@gmail.com>
2022-11-01 23:19:26 +0300

    Added vk dialogflow;

--- tg_bot.py
+++ tg_bot.py
@@ -1,6 +1,14 @@
+import os
+import logging
+from dotenv import load_dotenv
 from telegram import Update
-from telegram.ext import ContextTypes
+from telegram.ext import ContextTypes, Application, CommandHandler, MessageHandler, filters
 from dialogflow import detect_intent_texts
+
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.DEBUG
+)
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -17,3 +25,19 @@ async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user_id = update.message.from_user.id
     if text:
         await update.message.reply_text(detect_intent_texts(text, user_id))
+
+
+def main():
+    logger = logging.getLogger(__name__)
+    load_dotenv()
+    tg_token = os.getenv('TELEGRAM_TOKEN')
+
+    application = Application.builder().token(tg_token).build()
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("help", help_command))
+    application.add_handler(MessageHandler(filters.TEXT, hello))
+    application.run_polling()
+
+
+if __name__ == '__main__':
+    main()
--- vk_bot.py
+++ vk_bot.py
@@ -1,6 +1,10 @@
+import os
+import logging
 import random
 import vk_api
+from dotenv import load_dotenv
 from vk_api.longpoll import VkLongPoll, VkEventType
+from dialogflow import detect_intent_texts
 
 
 def vk_bot(token):
@@ -16,9 +20,31 @@ def vk_bot(token):
             print(f'Text: {event.text}\n')
 
 
-def echo(event, vk_api):
+def echo(event, vk_api, answer):
     vk_api.messages.send(
         user_id=event.user_id,
-        message=event.text,
+        message=answer,
         random_id=random.randint(1, 1000)
     )
+
+
+def main():
+    logger = logging.getLogger(__name__)
+    load_dotenv()
+    vk_token = os.getenv('VK_TOKEN')
+
+    vk_session = vk_api.VkApi(token=vk_token)
+    vk = vk_session.get_api()
+    longpoll = VkLongPoll(vk_session)
+
+    def vk_loop():
+        for event in longpoll.listen():
+            if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+                answer = detect_intent_texts(event.message, event.user_id)
+                echo(event, vk, answer)
+
+    vk_loop()
+
+
+if __name__ == '__main__':
+    main()
