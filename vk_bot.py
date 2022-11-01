d61011397ac23f5195e4202427d20794dadcdb37
gennadynemchin@gmail.com <gennadynemchin@gmail.com>
2022-11-01 21:48:06 +0300

    Added vk echobot;

--- vk_bot.py
+++ vk_bot.py
@@ -1,4 +1,5 @@
 import os
+import random
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 from dotenv import load_dotenv
@@ -17,5 +18,26 @@ def vk_bot(token):
             print(f'Text: {event.text}\n')
 
 
-load_dotenv()
-vk_bot(os.getenv('VK_TOKEN'))
+def echo(event, vk_api):
+    vk_api.messages.send(
+        user_id=event.user_id,
+        message=event.text,
+        random_id=random.randint(1, 1000)
+    )
+
+
+def main():
+    load_dotenv()
+    vk_token = os.getenv('VK_TOKEN')
+    vk_session = vk_api.VkApi(token=vk_token)
+    vk = vk_session.get_api()
+    longpoll = VkLongPoll(vk_session)
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            echo(event, vk)
+
+
+if __name__ == "__main__":
+    main()
+
+
