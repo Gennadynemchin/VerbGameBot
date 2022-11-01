9438c9d5b0cf4cc68eab570087762411317d5c31
gennadynemchin@gmail.com <gennadynemchin@gmail.com>
2022-11-01 21:52:31 +0300

    Added vk echobot;

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import os
 import logging
-from telegram.ext import Application, CommandHandler, MessageHandler
+import vk_api
+from vk_bot import echo
+from telegram.ext import Application, CommandHandler, MessageHandler, filters
 from tg_bot import start, help_command, hello
+from vk_api.longpoll import VkLongPoll, VkEventType
 from dotenv import load_dotenv
 
 
@@ -15,13 +18,18 @@ def main():
     load_dotenv()
     tg_token = os.getenv('TELEGRAM_TOKEN')
     vk_token = os.getenv('VK_TOKEN')
-    application = Application.builder().token(token).build()
+    application = Application.builder().token(vk_token).build()
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help_command))
     application.add_handler(MessageHandler(filters.TEXT, hello))
-    # application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
-
     application.run_polling()
+
+    vk_session = vk_api.VkApi(token=vk_token)
+    vk = vk_session.get_api()
+    longpoll = VkLongPoll(vk_session)
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            echo(event, vk)
 
 
 if __name__ == '__main__':
--- vk_bot.py
+++ vk_bot.py
@@ -1,8 +1,6 @@
-import os
 import random
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
-from dotenv import load_dotenv
 
 
 def vk_bot(token):
@@ -24,20 +22,3 @@ def echo(event, vk_api):
         message=event.text,
         random_id=random.randint(1, 1000)
     )
-
-
-def main():
-    load_dotenv()
-    vk_token = os.getenv('VK_TOKEN')
-    vk_session = vk_api.VkApi(token=vk_token)
-    vk = vk_session.get_api()
-    longpoll = VkLongPoll(vk_session)
-    for event in longpoll.listen():
-        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            echo(event, vk)
-
-
-if __name__ == "__main__":
-    main()
-
-
